12b29e6c8d93780708880e9576a084b511e2d77e
Vlad Boiko <acc.v.boiko@gmail.com>
2026-02-24 01:29:20 +0300

    feat: add SnsService and AbstractSnsService

--- src/async_sqs_consumer/__init__.py
+++ src/async_sqs_consumer/__init__.py
@@ -4,13 +4,16 @@ from ._types import MessageHandler
 from .consumer import SqsConsumer
 from .event import Event
 from .message_bus import MessageBus
+from .sns_service import AbstractSnsService, SnsService
 from .sqs_service import AbstractSqsService, SqsService
 
 __all__ = [
+    "AbstractSnsService",
     "AbstractSqsService",
     "Event",
     "MessageBus",
     "MessageHandler",
+    "SnsService",
     "SqsConsumer",
     "SqsService",
 ]
